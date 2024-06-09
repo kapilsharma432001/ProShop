8701f6ea398f82048b62f1b0d44b38bb3de2bd91
Kapil <kapil.sharma1@vvdntech.in>
2024-06-09 19:15:55 +0530

    Backend User Authentication

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from datetime import timedelta
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from django.contrib.auth.models import User
 from .models import *
 
 
@@ -6,3 +9,38 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only = True)
+    _id = serializers.SerializerMethodField(read_only = True)
+    isAdmin = serializers.SerializerMethodField(read_only = True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', '_id', 'isAdmin']
+
+
+    def get_isAdmin(self, obj):
+        isAdmin = obj.is_staff
+        return isAdmin
+    
+    def get__id(self, obj):
+        _id = obj.id
+        return _id
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == "":
+            return obj.email
+        return name
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only = True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name', '_id', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
+
