3db15d81fd0db8722ef403277f073b16c8b82d6e
Kapil <kapil.sharma1@vvdntech.in>
2024-06-10 16:56:27 +0530

    User Login Reducer and Action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
-
+import LoginScreen from "./screens/LoginScreen";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -17,6 +17,7 @@ function App() {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/login" element={<LoginScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id?" element={<CartScreen />} />
           </Routes>
